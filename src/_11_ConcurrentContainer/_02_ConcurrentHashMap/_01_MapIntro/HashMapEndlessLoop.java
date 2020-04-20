15f761e053030ad5b2ecd55470db057f905dc725
ZHENGQING ZHU <zhuzhengqing321@qq.com>
2020-04-20 20:13:25 +0800

    no message

---
+++ src/_11_ConcurrentContainer/_02_ConcurrentHashMap/_01_MapIntro/HashMapEndlessLoop.java
@@ -0,0 +1,27 @@
+package _11_ConcurrentContainer._02_ConcurrentHashMap._01_MapIntro;
+
+/*
+ * 演示在Java1.7下, HashMap在多线程下造成死循环并导致CPU利用率100%的情况;
+ * 核心原因在于, HashMap在多线程下同时扩容的时候, 可能会造成循环链表, 导致CPU利用率100%;
+ * 详细分析: https://coolshell.cn/articles/9606.html
+ */
+
+import java.util.HashMap;
+
+public class HashMapEndlessLoop {
+    private static HashMap<Integer, String> map = new HashMap<>(2, 1.5f);
+
+    public static void main(String[] args) {
+        map.put(5, "C");
+        map.put(7, "B");
+        map.put(3, "A");
+        new Thread(() -> {
+            map.put(15, "D");
+            System.out.println(map);
+        }, "Thread-1").start();
+        new Thread(() -> {
+            map.put(1, "E");
+            System.out.println(map);
+        }, "Thread-2").start();
+    }
+}
---
+++ src/_11_ConcurrentContainer/_02_ConcurrentHashMap/_02_ConcurrentHashMapStructure/ConcurrentHashMapStructure.java
@@ -0,0 +1,13 @@
+package _11_ConcurrentContainer._02_ConcurrentHashMap._02_ConcurrentHashMapStructure;
+
+/*
+ * ConcurrentHashMap的结构:
+ * ConcurrentHashMap在Java1.7和Java8中的结构是有区别的:
+ * 1. 在Java1.7中:
+ *
+ */
+
+public class ConcurrentHashMapStructure {
+    public static void main(String[] args) {
+    }
+}
