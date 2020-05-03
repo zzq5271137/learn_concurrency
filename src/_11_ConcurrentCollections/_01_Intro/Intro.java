e93fee8c6068ea7dcf46057e9b8c5192837aee3f
ZHENGQING ZHU <zhuzhengqing321@qq.com>
2020-05-03 09:54:05 +0800

    no message

--- src/_11_ConcurrentCollections/_01_Intro/Intro.java
+++ src/_11_ConcurrentCollections/_01_Intro/Intro.java
@@ -31,8 +31,9 @@ package _11_ConcurrentCollections._01_Intro;
  * 3. 阶段三: ConcurrentHashMap和CopyOnWriteArrayList
  *    这俩就是用来取代同步容器的(即Collections.synchronizedList和Collections.synchronizedMap);
  *    因为在绝大多数的情况下, 这两个的性能都比同步容器要好; 唯一例外的是, 对于list来说, 如果一个list经常被修改(经常被写入),
- *    那么Collections.synchronizedList的性能要优于CopyOnWriteArrayList, 因为CopyOnWriteArrayList适用于读多写少的场景;
- *    但是对于ConcurrentHashMap来说, 它几乎没有例外的在所有场景的性能都优于Collections.synchronizedMap;
+ *    那么Collections.synchronizedList的性能要优于CopyOnWriteArrayList, 因为CopyOnWriteArrayList适用于读多写少的场景,
+ *    因为它每次写入, 都会完整复制整个链表; 但是对于ConcurrentHashMap来说,
+ *    它几乎没有例外的在所有场景的性能都优于Collections.synchronizedMap;
  */
 
 public class Intro {
