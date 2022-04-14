3acbe6684f719d39181847f195f96e20e5c91d57
朱正清 <zhengqing.zhu@tcl.com>
2022-04-14 22:30:33 +0800

    1

--- src/_10_Immutable/StringPoolDemos/StringPoolIntro.java
+++ src/_10_Immutable/StringPoolDemos/StringPoolIntro.java
@@ -7,6 +7,7 @@ package _10_Immutable.StringPoolDemos;
  * 3. https://blog.csdn.net/weixin_43194122/article/details/88894818
  * 4. https://blog.csdn.net/tyyking/article/details/82496901
  * 5. https://www.jianshu.com/p/8966c51e9728
+ * 6. https://blog.csdn.net/SEU_Calvin/article/details/52291082
  */
 
 public class StringPoolIntro {
